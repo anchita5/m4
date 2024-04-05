64b990ab5583739733827ca3850d20621f6c3abc
Anchita Kakria <anchita585@gmail.com>
2024-04-05 18:44:36 -0400

    created a fucntion named processflight

--- m4.cpp
+++ m4.cpp
@@ -4,109 +4,97 @@
 
 #pragma warning (disable : 4996)
 
-struct AirlineFlights
+struct FlightDestination
 {
-    char airlineName[50];
-    char flightSource[50];
-    char flightDestination[50];
-    double flightFee;
+    char source[50];
+    char destination[50];
+    char airline[50];
+    int fare;
 };
 
-void displayLeastFareDetails();
 
-int processFlight(char* fileName, struct AirlineFlights* flightData, int* totalCount);
-
-int parseLine(char* line, char* source, char* destination, double* fare);
+int processFlight(char fileName[40], struct FlightDestination* structOfFlightsArray, int* totalCount);
 
 int main(void)
 {
-    FILE* flightsFile = NULL;
-    char fileName[100];
-    char flightsArray[100][50];
-    struct AirlineFlights flightsData[100];
-    int flightCounter = 0;
+ 
+    FILE* fpFlights = NULL;
 
-    flightsFile = fopen("teams.txt", "r"); 
+    char fileName[100] = "";
+    char flightsArray[100][50] = { "" };
 
-    if (flightsFile == NULL) 
+    struct FlightDestination arrayOflights[100] = {};
+
+    int countFlights = 0;
+
+    fpFlights = fopen("flights.txt", "r"); // Open flightsFile
+
+    if (fpFlights == NULL) // Check if file opened
     {
         printf("Error: Couldn't open file");
         return -1;
     }
 
-    do
-    {
-        char airline[100];
-        fgets(fileName, 50, flightsFile);
-        fileName[strcspn(fileName, "\n")] = '\0';
-        sscanf(fileName, "%[^.]", airline); 
-        strcpy(flightsArray[flightCounter], airline);
-        printf("%s\n", airline);
-        flightCounter++;
-    } while (!feof(flightsFile));
+    // Read file
 
-    int flightsFileClose = fclose(flightsFile);
+    while (fgets(fileName, 50, fpFlights) != NULL)
+    {
+        char flightName[100];
+        fileName[strcspn(fileName, "\n")] = '\0';
+
+        sscanf(fileName, "%[^.]", flightName); 
+
+        strcpy(flightsArray[countFlights], flightName);
+
+         printf("%s\n", flightName);
+        countFlights++;
+    }
+
+    int flightsFileClose = fclose(fpFlights);
     if (flightsFileClose != 0)
     {
         printf("File couldn't close");
         return -1;
     }
 
-    
-    int totalCount = 0;
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%s ", flightsArray[i]);
-        processFlight(flightsArray[i], flightsData, &totalCount);
-    }
-
-    printf("%d\n", totalCount);
     return 0;
 }
 
-
-int processFlight(char* fileName, struct AirlineFlights* flightData, int* totalCount)
+int processFlight(char fileName[40], struct FlightDestination* arrayOflights, int* totalCount)
 {
-    FILE* airlineFIle = NULL;
-    char currentLine[50] = "";
-    char source[20] = "";
-    char destination[20] = "";
-    double fare = 0;
+    FILE* flightsFile = NULL;
     char filePath[50] = "";
 
+    char fileLine[50] = "";
+    char source[50] = "";
+    char destination[50] = "";
+    int fare = 0;
+  
+
     strcpy(filePath, fileName);
+
     strcat(filePath, ".txt");
 
-    airlineFIle = fopen(filePath, "r");
+    flightsFile = fopen(filePath, "r");
 
-    if (airlineFIle == NULL)
+    if (flightsFile == NULL)
     {
         printf("Couldn't open file");
+
         return -1;
     }
 
-    do
+    while (fgets(fileLine, 50, flightsFile) != NULL && *totalCount < 101)
     {
-        fgets(currentLine, 50, airlineFIle);
-        currentLine[strcspn(currentLine, "\n")] = '\0';
-        parseLine(currentLine, source, destination, &fare);
-        (*totalCount)++;
-    } while (!feof(airlineFIle) && *totalCount < 100);
+        fileLine[strcspn(fileLine, "\n")] = '\0';
 
-    int fileClose = fclose(airlineFIle);
-    if (fileClose != 0)
-    {
-        printf("Coulnd't close file");
-    }
+        if (strlen(fileName) < 2)
+        {
+            continue;
+        }
 
     return 0;
 }
 
-// parseLine function
-int parseLine(char* line, char* source, char* destination, double* fare)
-{
-    sscanf(line, "%[^-] - %[^,], %lf", source, destination, fare);
-    printf("Source:%s, Destination: %s, Fare: %lf \n", source, destination, *fare);
-    return 0;
-}
+
+
